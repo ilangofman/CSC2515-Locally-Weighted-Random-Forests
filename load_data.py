d707ab25b5f7920e31af655734df9a582cb8cdef
ilangofman <igofman99@gmail.com>
2022-11-28 12:34:50 -0500

    Add load data and baseline performance

---
+++ load_data.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def load_heart_data(file_path):
+	data =  pd.read_csv(file_path, header=0)
+	target = data['target']
+	X = data.drop(columns=['target'])
+
+	X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.2, random_state=6)
+
+	return X_train.to_numpy(), X_test.to_numpy(), y_train.to_numpy(), y_test.to_numpy()
+
+	
+def load_tweet_data(file_path):
+    pass
