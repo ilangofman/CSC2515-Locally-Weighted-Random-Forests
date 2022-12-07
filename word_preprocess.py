e6c9f296bd621afc013b4cc2f327dc3e180fa6d5
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2022-12-07 17:10:17 -0500

    Add lemmatize

--- word_preprocess.py
+++ word_preprocess.py
@@ -2,6 +2,7 @@ from string import punctuation
 
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from num2words import num2words
 from spellchecker import SpellChecker
 
@@ -16,6 +17,16 @@ try:
     nltk.data.find('corpora/stopwords')
 except:
     nltk.download('stopwords')
+
+try:
+    nltk.data.find('corpora/wordnet')
+except:
+    nltk.download('wordnet')
+
+try:
+    nltk.data.find('corpora/omw-1.4')
+except:
+    nltk.download('omw-1.4')
 
 def string_to_token(test_string):
     return nltk.word_tokenize(test_string)
@@ -37,8 +48,9 @@ def translate_numbers(token_str):
 def num_toeknize(token_str):
     return ["NUMBER" if tok.isnumeric() else tok for tok in token_str]
 
-def lematize(test_string):
-    pass
+def lemmatize(token_str):
+    lemma = WordNetLemmatizer()
+    return [lemma.lemmatize(tok) if tok!="NUMBER" else "NUMBER" for tok in token_str]
 
 def token_to_string(token_str):
     return " ".join(token_str)
@@ -48,12 +60,12 @@ def clean_string(test_string):
     token_str = remove_capitals(token_str)
     token_str = remove_punct_stop(token_str)
     token_str = correct_spelling(token_str)
-    token_str = translate_numbers(token_str)
-    # token_str = num_toeknize(token_str)
-
+    # token_str = translate_numbers(token_str)
+    token_str = num_toeknize(token_str)
+    token_str = lemmatize(token_str)
     test_string = token_to_string(token_str)
     
 
     return test_string
 
-print(clean_string("The 23 quick and brown Fox, jumps over the lazy Dog."))
+print(clean_string("The 23 quick and brown Fox, jumps over the lazy Dog testing some verbs running sprint tasted saw."))
