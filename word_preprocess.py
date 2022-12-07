5e531e7ef7faaf01e1b51dccfc7fe84725855cf3
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2022-12-07 16:53:24 -0500

    switch to list comprehension

--- word_preprocess.py
+++ word_preprocess.py
@@ -1,9 +1,11 @@
+from string import punctuation
+
 import nltk
-from load_data import load_tweet_data
-import string
+from nltk.corpus import stopwords
 from num2words import num2words
 from spellchecker import SpellChecker
 
+from load_data import load_tweet_data
 
 try:
     nltk.data.find('tokenizers/punkt')
@@ -19,37 +21,21 @@ def string_to_token(test_string):
     return nltk.word_tokenize(test_string)
 
 def remove_capitals(token_str):
-    for i in range(len(token_str)):
-        token_str[i] = token_str[i].lower()
-    return token_str
+    return [tok.lower() for tok in token_str]
 
-def remove_punctutation(token_str):
-    new_token_str = []
-    for i in range(len(token_str)):
-        if token_str[i] not in string.punctuation:
-            new_token_str.append(token_str[i])
-    return new_token_str
-
-
-def remove_stopwords(token_str):
-    stop_words = nltk.corpus.stopwords.words('english')
-    new_token_str = []
-    for i in range(len(token_str)):
-        if token_str[i] not in stop_words:
-            new_token_str.append(token_str[i])
-    return new_token_str
+def remove_punct_stop(token_str):
+    return [tok for tok in token_str if \
+        (tok not in stopwords.words('english')) and (tok not in punctuation)]
 
 def correct_spelling(token_str):
     spell = SpellChecker()
-    for i in range(len(token_str)):
-        token_str[i] = spell.correction(token_str[i])
-    return token_str
+    return [spell.correction(tok) for tok in token_str]
 
 def translate_numbers(token_str):
-    for i in range(len(token_str)):
-        if token_str[i].isnumeric():
-            token_str[i] = num2words(token_str[i])
-    return token_str
+    return [num2words(tok) if tok.isnumeric() else tok for tok in token_str]
+
+def num_toeknize(token_str):
+    return ["NUMBER" if tok.isnumeric() else tok for tok in token_str]
 
 def lematize(test_string):
     pass
@@ -60,14 +46,14 @@ def token_to_string(token_str):
 def clean_string(test_string):
     token_str = string_to_token(test_string)
     token_str = remove_capitals(token_str)
-    token_str = remove_punctutation(token_str)
-    token_str = remove_stopwords(token_str)
+    token_str = remove_punct_stop(token_str)
     token_str = correct_spelling(token_str)
     token_str = translate_numbers(token_str)
+    # token_str = num_toeknize(token_str)
 
     test_string = token_to_string(token_str)
     
 
     return test_string
 
-print(clean_string("The 23 kuick and brown Fox, jumps over the lazy Dog."))
+print(clean_string("The 23 quick and brown Fox, jumps over the lazy Dog."))
